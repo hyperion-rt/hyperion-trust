ee8d8783dd6d74faeba4cfc882f8573827ae5094
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-12-12 15:48:13 +0100

    Tidy up and add script to compute SEDs

--- benchmark_1/effgrain/extract_images.py
+++ benchmark_1/effgrain/extract_images.py
@@ -5,16 +5,18 @@ from astropy.io import fits
 
 for tau in [0.1, 1.0, 20.]:
 
-	input_file = 'bm1_slab_eff_tau{tau:05.2f}_images.rtout'.format(tau=tau)
+    input_file = 'bm1_slab_effgrain_tau_{tau:05.2f}_images.rtout'.format(tau=tau)
 
-	m = ModelOutput(input_file)
+    print(input_file)
 
-	for iincl, theta in enumerate([0, 30, 60, 90, 120, 180]):
+    m = ModelOutput(input_file)
 
-		image = m.get_image(inclination=iincl, units='MJy/sr', distance=10. * kpc)
+    for iincl, theta in enumerate([0, 30, 60, 90, 120, 150, 180]):
 
-		for iwav, wav in enumerate([0.165, 0.570, 21.3, 161.6]):
+        image = m.get_image(inclination=iincl, units='MJy/sr', distance=10. * kpc)
 
-			output_file = 'bm1_hyperion/bm1_slab_effgrain_theta_{theta:03d}_wave_{wav:05.1f}.fits'.format(theta=theta, wav=wav)
+        for iwav, wav in enumerate([0.165, 0.570, 21.3, 161.6]):
 
-			fits.writeto(output_file, image.val[:, :, iwav], clobber=True)
+            output_file = 'bm1_slab_effgrain_tau_{tau:05.2f}_theta_{theta:03d}_wave_{wav:05.1f}.fits'.format(tau=tau, theta=theta, wav=wav)
+
+            fits.writeto(output_file, image.val[:, :, iwav], clobber=True)
---
+++ benchmark_1/effgrain/setup_seds.py
@@ -0,0 +1,30 @@
+import numpy as np
+from hyperion.model import Model
+from hyperion.dust import SphericalDust
+from hyperion.util.constants import pc
+
+for tau_v in [0.1, 1.0, 20.0]:
+
+    m = Model.read('bm1_slab_eff_tau{0:05.2f}_temperature.rtout'.format(tau_v), only_initial=False)
+
+    m.set_n_initial_iterations(0)
+
+    del m.n_photons['initial']
+    del m.n_photons['last']
+
+    i = m.add_peeled_images(sed=True, image=False)
+    i.set_viewing_angles([0., 30., 60., 90., 120., 150., 180.], [0., 0., 0., 0., 0., 0., 0.])
+
+    # Set up monochromatic mode
+    wavelengths = np.loadtxt('wave_grid_bm1_res5.dat')
+    m.set_monochromatic(True, wavelengths=wavelengths)
+
+    # Use raytracing
+    m.set_raytracing(True)
+
+    # Set up number of photons
+    m.set_n_photons(imaging_sources=1e7, imaging_dust=1e7,
+                    raytracing_sources=1, raytracing_dust=1e7)
+
+    # Write out and run
+    m.write('bm1_slab_effgrain_tau_{0:05.2f}_seds.rtin'.format(tau_v), overwrite=True)
